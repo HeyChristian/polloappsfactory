d42ffb191c58564e1639734bcc2342715572a3ea
Christian Vazquez <colapsov@gmail.com>
2014-08-18 16:00:25 -0500

    Initial Commit

---
+++ PolloAppsFactory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PolloAppsFactory
+//
+//  Created by Christian Vazquez on 8/18/14.
+//  Copyright (c) 2014 Christian Vazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PolloAppsFactoryTests/PolloAppsFactoryTests.m
@@ -0,0 +1,34 @@
+//
+//  PolloAppsFactoryTests.m
+//  PolloAppsFactoryTests
+//
+//  Created by Christian Vazquez on 8/18/14.
+//  Copyright (c) 2014 Christian Vazquez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PolloAppsFactoryTests : XCTestCase
+
+@end
+
+@implementation PolloAppsFactoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
